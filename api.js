3817286cda2120b62ac15875a2ee12daedd3774d
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-28 09:50:45 -0400

    updated initmap function

--- api.js
+++ api.js
@@ -67,10 +67,20 @@ var service;
 var infowindow;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -25.344, lng: 131.036},
-        zoom: 15
-    });
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -25.344, lng: 131.036},
+//         zoom: 15
+//     });
     // var sydney = new google.maps.LatLng(-33.867, 151.195);
 
     // infowindow = new google.maps.InfoWindow();
@@ -81,7 +91,7 @@ function initMap() {
     // var request = {
     //     query: 'Museum of Contemporary Art Australia',
     //     fields: ['name', 'geometry'],
-};
+// };
 
 // service = new google.maps.places.PlacesService(map);
 
