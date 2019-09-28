3c34d1ddb13e91ccc372f21c731c690c6e8ea59d
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-28 08:52:15 -0400

    adding map in

--- api.js
+++ api.js
@@ -5,7 +5,11 @@ let restaurant = document.getElementById('restaurant');
 let bar = document.getElementById('bar');
 let locationSearch; 
 let place; 
-let locationArr = [];
+let resultsArr = [];
+let randomPick;
+let groupArr = [];
+
+const randomize = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 // let service = new google.maps.places.PlacesService(map);
 
 // let placeFilter = typeOfPlace.id; 
@@ -20,176 +24,82 @@ google.maps.event.addListener(ac, 'place_changed', function() {
 
 $(".create-hangout").on("click", function(event) {
     event.preventDefault();
-    // if (cafe.checked = true) {
-    //     typeOfPlace = cafe; 
-    // } else if (restaurant.checked = true) {
-    //     cafe.checked = false; 
-    //     bar.checked = false; 
-    //     typeOfPlace = restaurant; 
-    // } else if (bar.checked = true) {
-    //     cafe.checked = false; 
-    //     restaurant.checked = false; 
-    //     typeOfPlace = bar; 
-    // }
     
-    // let placeFilter = typeOfPlace.id; 
-    // google.maps.event.addListener(ac, 'place_changed', function() {
-    //     let place = ac.getPlace();
-    //     // console.log(place.formatted_address);
-    //     // console.log(place.url);
-    //     // console.log(place.geometry.location);
         locationSearch = place.geometry.location; 
         let lat = place.geometry.location.lat();
         let lng = place.geometry.location.lng();
-        console.log(lat);
-        console.log(lng);
-        // locationSearch.push(locationArr);
-        // let location= locationSearch.replace(/[()]/g, '');
-        // let location= locationSearch.slice(1, -1);
-        console.log(location);
         const APIKEY = "AIzaSyDWLRgKxz3nTinzcUXCyjM1DNpe9e4_g2w";
-        let initialQueryURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${APIKEY}&location=${lat},${lng}&radius=1000&type=restaurant`;
+        let initialQueryURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${APIKEY}&location=${lat},${lng}&radius=1000&type=restaurant`;
         $.ajax({
             url: initialQueryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
-            for (let i = 0; i < response.length; i++) {
-                console.log(response.results[i].name);
-                console.log(response.results[i].opening_hours);
-                console.log(response.results[i].photos);
+            // console.log(response);
+            for (let i = 0; i < response.results.length; i++) {
+                let place_id = response.results[i].place_id;
+                // let place_name = response.results[i].name;
+                // let open = response.results[i].opening_hours.open_now;
+                // let photoResult = response.results[i].photos[0].html_attributions;
+                // let address = response.results[i].vicinity;
+                resultsArr.push(place_id);
+                randomPick = resultsArr[randomize(0, resultsArr.length - 1)];
             }
+            console.log(randomPick);
+            let pickedQueryURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=${APIKEY}&place_id=${randomPick}`;
+            $.ajax({
+                url: pickedQueryURL,
+                method: "GET"
+            }).then(function(response) {
+                let pickLat = response.result.geometry.location.lat;
+                let pickLng = response.result.geometry.location.lng;
+                let pickName = response.result.name;
+                let pickAddress = response.result.formatted_address;
+                let pickIcon = response.result.icon;
+                let pickOpenNow = response.result.opening_hours.open_now;
+                let pickWebsite = response.result.website;
+                // $("#")
+            });
         });
-            // let request = {
-            //     location: locationSearch, 
-            //     radius: 5000, 
-            //     types: [ 'restaurant' ]
-            // };
-            // service.search(request);
-            // console.log(request);
-
 });
 
-// Google Maps Initializer
-// function initMap() {
-//     console.log("working");
-//     // let location = venue0;
-//     map = new google.maps.Map(document.getElementById('map'), {
-//       center: {lat: -34.397, lng: 150.644},
-//           zoom: 15
-//       });
-    // searchResults.forEach(function(e, i) {
-    //   makeMarker(locationResult[i]);
-    // })
-// }
+var map;
+var service;
+var infowindow;
 
-// initMap();
+function initMap() {
+var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-// gMaps Marker Creation
-// function makeMarker(locationResult) {
-//     var marker = new google.maps.Marker({position: searchResults.coords, map: map});
-//     let infoWindow = new google.maps.InfoWindow({
-//       content:searchResults.content
-//     });
-//     marker.addListener('click', function() {
-//       infoWindow.open(map, marker);
-//     })
-// }
+infowindow = new google.maps.InfoWindow();
 
-// let locationInput = document.getElementById('enterLocation');
-// let map;
-// function initMap() {
-//  // Create the map.
-//  let pyrmont = {lat: -33.866, lng: 151.196};
-//  map = new google.maps.Map(document.getElementById('map'), {
-//    center: pyrmont,
-//    zoom: 17
-//  });
+map = new google.maps.Map(
+    document.getElementById('map'), {center: sydney, zoom: 15});
 
- // Create the places service.
-//  let service = new google.maps.places.PlacesService(map);
-//  let getNextPage = null;
-//  let moreButton = document.getElementById('more');
-//  moreButton.onclick = function() {
-//    moreButton.disabled = true;
-//    if (getNextPage) getNextPage();
-//  };
+var request = {
+    query: 'Museum of Contemporary Art Australia',
+    fields: ['name', 'geometry'],
+};
 
- // Perform a nearby search.
-//  service.nearbySearch(
-//      {location: locationInput, radius: 1000, type: ['restaurant']},
-//      function(results, status, pagination) {
-//        if (status !== 'OK') return;
-      //  createMarkers(results);
-      //  moreButton.disabled = !pagination.hasNextPage;
-      //  getNextPage = pagination.hasNextPage && function() {
-      //    pagination.nextPage();
-      //  };
-    //  });
+service = new google.maps.places.PlacesService(map);
 
+service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+    }
 
-// function createMarkers(places) {
-//  let bounds = new google.maps.LatLngBounds();
-//  let placesList = document.getElementById('places');
-//  for (let i = 0, place; place = places[i]; i++) {
-//     let image = {
-//       url: place.icon,
-//       size: new google.maps.Size(71, 71),
-//       origin: new google.maps.Point(0, 0),
-//       anchor: new google.maps.Point(17, 34),
-//       scaledSize: new google.maps.Size(25, 25)
-//     };
-//     let marker = new google.maps.Marker({
-//       map: map,
-//       icon: image,
-//       title: place.name,
-//       position: place.geometry.location
-//     });
-//     let li = document.createElement('li');
-//     li.textContent = place.name;
-//     placesList.appendChild(li);
-//     bounds.extend(place.geometry.location);
-//   }
-//   map.fitBounds(bounds);
-// }
+    map.setCenter(results[0].geometry.location);
+    }
+});
+}
 
-// let map;
-// let service;
-// let infowindow;
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
 
-// function initMap() {
-//     let sydney = new google.maps.LatLng(-33.867, 151.195);
-
-//     infowindow = new google.maps.InfoWindow();
-
-//     map = new google.maps.Map(
-//         document.getElementById('map'), {center: sydney, zoom: 15});
-
-//     let request = {
-//         query: 'Museum of Contemporary Art Australia',
-//         fields: ['name', 'geometry'],
-//     };
-
-//     service = new google.maps.places.PlacesService(map);
-
-//     service.findPlaceFromQuery(request, function(results, status) {
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//             createMarker(results[i]);
-//         }
-//         map.setCenter(results[0].geometry.location);
-//     }
-//     });
-// }
-
-// function createMarker(place) {
-//     var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//     });
-
-//     google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//     });
-// }
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
