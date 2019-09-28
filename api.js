f7d5db65f69aca75843259992ff7896bec954292
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-28 09:40:02 -0400

    commented out other functions for map to display

--- api.js
+++ api.js
@@ -83,27 +83,27 @@ function initMap() {
     //     fields: ['name', 'geometry'],
 };
 
-service = new google.maps.places.PlacesService(map);
+// service = new google.maps.places.PlacesService(map);
 
-service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-    }
+// service.findPlaceFromQuery(request, function(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < results.length; i++) {
+//             createMarker(results[i]);
+//     }
 
-    map.setCenter(results[0].geometry.location);
-    }
-});
-}
+//     map.setCenter(results[0].geometry.location);
+//     }
+// });
+// }
 
-function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+// function createMarker(place) {
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//     });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-}
+//     google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent(place.name);
+//         infowindow.open(map, this);
+//     });
+// }
